184b704904b724147d24bfe926dfeb48ab781535
Kacper <kacper.wroblewski651@gmail.com>
2022-12-12 19:21:04 +0100

    First ideas about app

---
+++ BoardGameService/BoardGameService/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using BoardGameService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BoardGameService.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<BoardGame> boardGames = new List<BoardGame>
+        {
+            new BoardGame(1, "Monopoly", 4, "Fajna gra rodzinna", "monopoly.jpg")
+
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AllBoardGames()
+        {
+            return View(boardGames);
+        }
+
+        public IActionResult Thread()
+        {
+            var boardGame = boardGames[0];
+
+            return View(boardGame);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BoardGameService/BoardGameService/Models/BoardGame.cs
@@ -0,0 +1,20 @@
+﻿namespace BoardGameService.Models
+{
+    public class BoardGame
+    {
+        public BoardGame(int id, string name, float rating, string description, string image)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Rating = rating;
+            this.Description = description;
+            this.Image = image;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Rating { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+    }
+}
