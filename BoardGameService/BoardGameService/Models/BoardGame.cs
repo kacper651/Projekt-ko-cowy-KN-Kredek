d2b34b273c4835f423eb119e32fbef5bd0740e4d
Kacper <kacper.wroblewski651@gmail.com>
2022-12-19 20:09:56 +0100

    Implemented DataBase and Adding Comments

--- BoardGameService/BoardGameService/Models/BoardGame.cs
+++ BoardGameService/BoardGameService/Models/BoardGame.cs
@@ -15,6 +15,6 @@
         public float Rating { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
-
+        public ICollection<Comment> Comments { get; set; }
     }
 }
---
+++ BoardGameService/BoardGameService/Models/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BoardGameService.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BoardGame>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Comment>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<BoardGame>()
+                .HasMany(x => x.Comments)
+                .WithOne(x => x.BoardGame)
+                .HasForeignKey(x => x.BoardGameId);
+        }
+
+        public DbSet<BoardGame> Boards { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
